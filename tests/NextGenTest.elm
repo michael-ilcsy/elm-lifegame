dd178b3cee3e301a68013107708693014bbf4f7d
michael-ilcsy <michael_ilcsy_git@yahoo.co.jp>
2020-07-12 20:52:11 +0900

    [WIP] テストのリファクタリング途中

--- tests/NextGenTest.elm
+++ tests/NextGenTest.elm
@@ -1,9 +1,8 @@
 module NextGenTest exposing (..)
 
-import Expect
+import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer)
 import Main exposing (Cell(..), nextGen)
-import Random
 import Random.List
 import Shrink
 import Test exposing (..)
@@ -14,49 +13,66 @@ all =
     describe "次世代のセル計算のテスト"
         [ describe "周りに8マス存在するセルのテスト"
             [ describe "生きているセルのテスト"
-                [ normalCellsTestHelper
-                    "周りの生きたセルが0のときは死滅すること"
-                    0
-                    Dead
-                , normalCellsTestHelper
-                    "周りの生きたセルが1のときは死滅すること"
-                    1
-                    Dead
-                , normalCellsTestHelper
-                    "周りの生きたセルが2のときは生存すること"
-                    2
-                    Alive
-                , normalCellsTestHelper
-                    "周りの生きたセルが3のときは生存すること"
-                    3
-                    Alive
-                , normalCellsTestHelper
-                    "周りの生きたセルが4のときは死滅すること"
-                    4
-                    Dead
-                , normalCellsTestHelper
-                    "周りの生きたセルが5のときは死滅すること"
-                    5
-                    Dead
-                , normalCellsTestHelper
-                    "周りの生きたセルが6のときは死滅すること"
-                    6
-                    Dead
-                , normalCellsTestHelper
-                    "周りの生きたセルが7のときは死滅すること"
-                    7
-                    Dead
-                , normalCellsTestHelper
-                    "周りの生きたセルが8のときは死滅すること"
-                    8
-                    Dead
-                ]
+                ([ ( 0, Dead )
+                 , ( 1, Dead )
+                 , ( 2, Alive )
+                 , ( 3, Alive )
+                 , ( 4, Dead )
+                 , ( 5, Dead )
+                 , ( 6, Dead )
+                 , ( 7, Dead )
+                 , ( 8, Dead )
+                 ]
+                    |> List.map (\( count, cell ) -> aliveNormalCellsTestHelper count cell)
+                )
+            , describe "死んでいるセルのテスト"
+                ([ ( 0, Dead )
+                 , ( 1, Dead )
+                 , ( 2, Dead )
+                 , ( 3, Alive )
+                 , ( 4, Dead )
+                 , ( 5, Dead )
+                 , ( 6, Dead )
+                 , ( 7, Dead )
+                 , ( 8, Dead )
+                 ]
+                    |> List.map (\( count, cell ) -> deadNormalCellsTestHelper count cell)
+                )
             ]
         ]
 
 
-normalCellsTestHelper : String -> Int -> Cell -> Test
-normalCellsTestHelper description aliveCellCount expectedCell =
+aliveNormalCellsTestHelper : Int -> Cell -> Test
+aliveNormalCellsTestHelper =
+    normalCellsTestHelper True
+
+
+deadNormalCellsTestHelper : Int -> Cell -> Test
+deadNormalCellsTestHelper =
+    normalCellsTestHelper False
+
+
+cellsTestDescription : Int -> Cell -> String
+cellsTestDescription count cell =
+    "周りの生きたセルが"
+        ++ String.fromInt count
+        ++ "のときは"
+        ++ (case cell of
+                Alive ->
+                    "生存"
+
+                Dead ->
+                    "死滅"
+           )
+        ++ "すること"
+
+
+normalCellsTestHelper : Bool -> Int -> Cell -> Test
+normalCellsTestHelper isTargetAlive aliveCellCount expectedCell =
+    let
+        description =
+            cellsTestDescription aliveCellCount expectedCell
+    in
     if aliveCellCount >= 9 then
         test description <|
             \_ ->
@@ -71,33 +87,7 @@ normalCellsTestHelper description aliveCellCount expectedCell =
             test =
                 fuzz aliveCellsFuzzer
                     description
-                    (\list ->
-                        let
-                            baseCells =
-                                [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ] ]
-                                    |> List.map
-                                        (\cells ->
-                                            List.map
-                                                (\number ->
-                                                    if List.member number (5 :: list) then
-                                                        Alive
-
-                                                    else
-                                                        Dead
-                                                )
-                                                cells
-                                        )
-
-                            actualCell =
-                                case nextGen baseCells of
-                                    [ [ _, _, _ ], [ _, a, _ ], [ _, _, _ ] ] ->
-                                        Just a
-
-                                    _ ->
-                                        Nothing
-                        in
-                        Expect.equal (Just expectedCell) actualCell
-                    )
+                    (cellsExpectation isTargetAlive expectedCell)
         in
         test
 
@@ -106,4 +96,40 @@ shuffledList : List Int -> Fuzzer (List Int)
 shuffledList list =
     Fuzz.custom
         (Random.List.shuffle list)
-        Shrink.noShrink
+        (Shrink.list Shrink.int)
+
+
+cellsExpectation : Bool -> Cell -> List Int -> Expectation
+cellsExpectation isTargetAlive expectedCell list =
+    let
+        aliveCellList =
+            if isTargetAlive then
+                5 :: list
+
+            else
+                list
+
+        baseCells =
+            [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ] ]
+                |> List.map
+                    (\cells ->
+                        List.map
+                            (\number ->
+                                if List.member number aliveCellList then
+                                    Alive
+
+                                else
+                                    Dead
+                            )
+                            cells
+                    )
+
+        actualCell =
+            case nextGen baseCells of
+                [ [ _, _, _ ], [ _, a, _ ], [ _, _, _ ] ] ->
+                    Just a
+
+                _ ->
+                    Nothing
+    in
+    Expect.equal (Just expectedCell) actualCell
